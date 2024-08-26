a61d3b5b47b33ca7895eccfccf08bae7a0358ad8
gang3133 <162407099+gang3133@users.noreply.github.com>
2024-08-26 15:31:24 +0900

    Add files via upload

---
+++ PID통신 model/PID통신 model/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace PID통신_model
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.DisconnectButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(739, 588);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(797, 0);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(439, 384);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(797, 397);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(439, 106);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1084, 516);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 60);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "send";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(777, 516);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(123, 60);
+            this.ConnectButton.TabIndex = 4;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            // 
+            // DisconnectButton
+            // 
+            this.DisconnectButton.Location = new System.Drawing.Point(906, 516);
+            this.DisconnectButton.Name = "DisconnectButton";
+            this.DisconnectButton.Size = new System.Drawing.Size(123, 60);
+            this.DisconnectButton.TabIndex = 5;
+            this.DisconnectButton.Text = "Disconnect";
+            this.DisconnectButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1257, 749);
+            this.Controls.Add(this.DisconnectButton);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button DisconnectButton;
+    }
+}
+
---
+++ PID통신 model/PID통신 model/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.IO.Ports;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Diagnostics;
+
+namespace PID통신_model
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort;
+        string dataIn;
+        private Stopwatch stopwatch = new Stopwatch();
+        private double timeInterval = 0.005;  // 5ms
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 시리얼 포트 설정
+            serialPort = new SerialPort("COM4", 9600, Parity.None, 8, StopBits.One);
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+
+            // 송신 버튼 클릭 이벤트 핸들러 등록
+            button1.Click += new EventHandler(SendData);  // button1은 송신 버튼입니다.
+                                                          // 'Connect' 버튼 클릭 이벤트 핸들러 등록
+            ConnectButton.Click += new EventHandler(ConnectButton_Click);  // 'ConnectButton'은 연결 버튼입니다.
+                                                                           // 'Disconnect' 버튼 클릭 이벤트 핸들러 등록
+            DisconnectButton.Click += new EventHandler(DisconnectButton_Click);  // 'DisconnectButton'은 연결 해제 버튼입니다.
+                                                                                 // 마우스 휠 이벤트 핸들러 등록
+            chart1.MouseWheel += new MouseEventHandler(Chart1_MouseWheel);
+            chart1.ChartAreas[0].AxisX.Title = "시간 (s)";
+            chart1.ChartAreas[0].AxisY.Title = "RPM";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 그래프 설정
+            chart1.Series.Clear(); // 기존 Series 제거
+            chart1.Series.Add("M1"); // 'M1' Series 추가
+            chart1.Series.Add("M2"); // 'M2' Series 추가
+            chart1.Series["M1"].ChartType = SeriesChartType.Line;
+            chart1.Series["M2"].ChartType = SeriesChartType.Line;
+            // X축과 Y축의 라벨 설정
+           
+        }
+
+        // 데이터 수신 이벤트
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            dataIn = serialPort.ReadLine();
+            if (this.IsHandleCreated)  // 컨트롤이 완전히 로드되었는지 확인
+            {
+                this.Invoke(new EventHandler(ShowData));
+            }
+        }
+
+        // Connect 버튼 클릭 이벤트
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                serialPort.Open();  // 시리얼 포트 연결
+                stopwatch.Start();  // 데이터 수집 시작
+            }
+        }
+
+        // Disconnect 버튼 클릭 이벤트
+        private void DisconnectButton_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();  // 시리얼 포트 연결 종료
+                stopwatch.Stop();  // 데이터 수집 종료
+            }
+        }
+
+
+        // 데이터 표시
+        private void ShowData(object sender, EventArgs e)
+        {
+            // 데이터 파싱
+            var data = dataIn.Split(new string[] { "M1: ", "rpm  M2: ", "rpm" }, StringSplitOptions.RemoveEmptyEntries);
+            if (data.Length >= 2)
+            {
+                // M1, M2 값을 찾아서 그래프에 추가
+                double m1Value, m2Value;
+                if (double.TryParse(data[0], out m1Value) && double.TryParse(data[1], out m2Value))
+                {
+                    double currentTime = stopwatch.Elapsed.TotalSeconds;
+                    chart1.Series["M1"].Points.AddXY(currentTime, m1Value);
+                    chart1.Series["M2"].Points.AddXY(currentTime, m2Value);
+
+                    // 수신한 데이터를 텍스트 박스에 표시
+                    textBox1.Text = $"M1: {m1Value} RPM, M2: {m2Value} RPM";
+                }
+            }
+
+            // 스크롤바 설정
+            chart1.ChartAreas[0].AxisX.ScrollBar.Enabled = true;
+            chart1.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
+            chart1.ChartAreas[0].AxisX.ScrollBar.Size = 10;
+
+            chart1.ChartAreas[0].CursorX.AutoScroll = true;
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisX.ScaleView.SizeType = DateTimeIntervalType.Number;
+            double viewSize = 5.0;  // view size of 20 seconds
+            double position = stopwatch.Elapsed.TotalSeconds;
+            double start = position > viewSize ? position - viewSize : 0;
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoom(start, position);
+        }
+
+        private void SendData(object sender, EventArgs e)
+        {
+            string dataOut = textBox2.Text;  // textBox2에서 데이터를 가져옵니다.
+            //string dataOut = "01E";
+            serialPort.WriteLine(dataOut);  // 가져온 데이터를 송신합니다.
+        }
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        // 마우스 휠 이벤트 핸들러
+        private void Chart1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            var chart = (Chart)sender;
+            var xAxis = chart.ChartAreas[0].AxisX;
+            var yAxis = chart.ChartAreas[0].AxisY;
+
+            try
+            {
+                if (e.Delta < 0) // Zoom out
+                {
+                    xAxis.ScaleView.ZoomReset();
+                    yAxis.ScaleView.ZoomReset();
+                }
+
+                if (e.Delta > 0) // Zoom in
+                {
+                    var xMin = xAxis.ScaleView.ViewMinimum;
+                    var xMax = xAxis.ScaleView.ViewMaximum;
+
+                    var yMin = yAxis.ScaleView.ViewMinimum;
+                    var yMax = yAxis.ScaleView.ViewMaximum;
+
+                    var posXStart = xAxis.PixelPositionToValue(e.Location.X) - (xMax - xMin) / 4;
+                    var posXFinish = xAxis.PixelPositionToValue(e.Location.X) + (xMax - xMin) / 4;
+
+                    var posYStart = yAxis.PixelPositionToValue(e.Location.Y) - (yMax - yMin) / 4;
+                    var posYFinish = yAxis.PixelPositionToValue(e.Location.Y) + (yMax - yMin) / 4;
+
+                    xAxis.ScaleView.Zoom(posXStart, posXFinish);
+                    yAxis.ScaleView.Zoom(posYStart, posYFinish);
+                }
+            }
+            catch { }
+        }
+    }
+}
